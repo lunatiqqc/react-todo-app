7a587a81e0591a13acb9e630ba8cf8af11c0d0c2
lunatiqqc <59181843+lunatiqqc@users.noreply.github.com>
2021-11-30 17:13:36 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useLayoutEffect, useState } from "react";
+
+import { BsFillTrashFill } from "react-icons/bs";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState();
+
+    const [newTodo, setNewTodo] = useState();
+
+    function handleAddTodo(todo) {
+        setTodos((prev) => {
+            return [...prev, todo];
+        });
+    }
+
+    function handleFormChange(e) {
+        setNewTodo((prev) => {
+            return { ...prev, [e.target.name]: e.target.value };
+        });
+    }
+
+    function handleFormSubmit(e) {
+        e.preventDefault();
+        fetch("https://jsonplaceholder.typicode.com/todos", {
+            method: "POST",
+            body: JSON.stringify(newTodo),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json);
+                handleAddTodo(json);
+            });
+    }
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/todos")
+            .then((res) => {
+                return res.json();
+            })
+            .then((json) => {
+                setTodos(json);
+            });
+    }, []);
+
+    console.log(todos);
+    return (
+        <div>
+            <article>
+                <form
+                    onSubmit={handleFormSubmit}
+                    onChange={handleFormChange}
+                    action=''
+                >
+                    <input name='title' type='text' />
+
+                    <button type='submit'>Add new todo</button>
+                </form>
+                <ul>
+                    {todos
+                        ?.sort((a, b) => {
+                            return a.id > b.id ? -1 : 1;
+                        })
+                        .map((todo, i) => {
+                            return (
+                                <li key={i}>
+                                    {todo.id} {todo.title} <BsFillTrashFill />
+                                </li>
+                            );
+                        })}
+                </ul>
+            </article>
+        </div>
+    );
 }
 
 export default App;
